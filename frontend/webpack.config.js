28e5bdeead29064caec7b0cc23c363fd3d6684c7
Ollebolle <ollesv@gmail.com>
2018-01-12 17:55:27 +0100

    Added webpack dashboard and cleaned webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,29 +1,30 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
-const dev = process.env.NODE_ENV !== 'production' && process.argv.indexOf('-p') === -1;
+const dev = process.env.NODE_ENV !== 'production' && process.argv.indexOf('-p') === -1
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, '/src/index.html'),
   filename: 'index.html',
-  inject: 'body',
-});
+  inject: 'body'
+})
 
 const DefinePluginConfig = new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify('production'),
-});
+  'process.env.NODE_ENV': JSON.stringify('production')
+})
 
 const UglifyJsPluginConfig = new webpack.optimize.UglifyJsPlugin({
   beautify: false,
   mangle: {
-    screw_ie8: true,
+    screw_ie8: true
   },
   compress: {
-    screw_ie8: true,
+    screw_ie8: true
   },
-  comments: false,
-});
+  comments: false
+})
 
 module.exports = {
   devServer: {
@@ -31,40 +32,35 @@ module.exports = {
     port: '2400',
     hot: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+      'Access-Control-Allow-Origin': '*'
+    }
   },
   entry: [
     'react-hot-loader/patch',
-    path.join(__dirname, '/src/index.jsx'),
+    path.join(__dirname, '/src/index.jsx')
   ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader'],
+        loaders: ['babel-loader']
       },
       {
         test: /\.sass$/,
-        loader: 'style-loader!css-loader!sass-loader',
+        loader: 'style-loader!css-loader!sass-loader'
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 10000
         }
-      },
-      {
-        test: /\.yaml$/,
-        include: path.resolve('data'),
-        loader: 'yaml'
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']
   },
   output: {
     filename: 'index.js',
@@ -75,6 +71,8 @@ module.exports = {
     HTMLWebpackPluginConfig,
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
+    new DashboardPlugin()
   ] :
   [HTMLWebpackPluginConfig, DefinePluginConfig, UglifyJsPluginConfig],
-};
+  devtool: dev ? 'eval-source-map' : false
+}
